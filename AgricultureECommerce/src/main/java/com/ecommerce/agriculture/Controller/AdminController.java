7742ae3b71704fa2978379f79c1f9081197b2246
soumyadip007 <soumyadip.note@gmail.com>
2020-01-30 22:54:53 +0530

    AdminController

--- AgricultureECommerce/src/main/java/com/ecommerce/agriculture/Controller/AdminController.java
+++ AgricultureECommerce/src/main/java/com/ecommerce/agriculture/Controller/AdminController.java
@@ -228,7 +228,7 @@ public class AdminController {
 		adminServiceImplementation.save(admin);
 		
 		// use a redirect to prevent duplicate submissions
-		return "redirect:/admin/userdetails";
+		return "redirect:/admin/add-admin";
 	}
 	
 	
