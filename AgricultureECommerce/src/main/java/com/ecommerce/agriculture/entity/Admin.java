133203445b01b80cd28340fec7fc58e3a8b96f5e
soumyadip007 <soumyadip.note@gmail.com>
2020-01-30 23:19:31 +0530

    Getter Setters Added

--- AgricultureECommerce/src/main/java/com/ecommerce/agriculture/entity/Admin.java
+++ AgricultureECommerce/src/main/java/com/ecommerce/agriculture/entity/Admin.java
@@ -74,4 +74,156 @@ public class Admin {
 	@Column(name = "longi")
 	private String longi;
 
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getUser() {
+		return user;
+	}
+
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+
+	public String getConfirmationToken() {
+		return confirmationToken;
+	}
+
+
+	public void setConfirmationToken(String confirmationToken) {
+		this.confirmationToken = confirmationToken;
+	}
+
+
+	public String getGender() {
+		return gender;
+	}
+
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+
+	public String getRole() {
+		return role;
+	}
+
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+
+	public String getLastseen() {
+		return lastseen;
+	}
+
+
+	public void setLastseen(String lastseen) {
+		this.lastseen = lastseen;
+	}
+
+
+	public String getMobile() {
+		return mobile;
+	}
+
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getLatt() {
+		return latt;
+	}
+
+
+	public void setLatt(String latt) {
+		this.latt = latt;
+	}
+
+
+	public String getLongi() {
+		return longi;
+	}
+
+
+	public void setLongi(String longi) {
+		this.longi = longi;
+	}
+
+	
+	
 }
