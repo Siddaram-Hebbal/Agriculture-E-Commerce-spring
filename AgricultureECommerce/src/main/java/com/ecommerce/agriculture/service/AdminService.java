68d350e99ff120c277c75a6d99b10a613c0e8a61
soumyadip007 <soumyadip.note@gmail.com>
2020-01-31 15:42:52 +0530

    AS

--- AgricultureECommerce/src/main/java/com/ecommerce/agriculture/service/AdminService.java
+++ AgricultureECommerce/src/main/java/com/ecommerce/agriculture/service/AdminService.java
@@ -3,7 +3,7 @@ package com.ecommerce.agriculture.service;
 import java.util.List;
 
 import com.ecommerce.agriculture.entity.Admin;
-import com.ecommerce.agriculture.entity.User;
+import com.ecommerce.agriculture.entity.Seller;
 /**
  * 
  * @author Soumyadip Chowdhury
