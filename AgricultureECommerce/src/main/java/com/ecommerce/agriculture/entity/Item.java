fe6dbe72a18f358d11cd18214aca7daeb3a34682
soumyadip007 <soumyadip.note@gmail.com>
2020-01-31 16:00:22 +0530

    ITEM JAVA

--- AgricultureECommerce/src/main/java/com/ecommerce/agriculture/entity/Item.java
+++ AgricultureECommerce/src/main/java/com/ecommerce/agriculture/entity/Item.java
@@ -60,6 +60,11 @@ public class Item {
 	@Column(name = "sellerAddress")
 	private String sellerAddress;
 
+	@Column(name = "sellerLatt")
+	private String sellerLatt;
+
+	@Column(name = "sellerLatt")
+	private String sellerLongi;
 	
 	@Column(name = "sellerMob")
 	private String sellerMob;
@@ -70,4 +75,166 @@ public class Item {
 	
 	@Column(name = "views")
 	private boolean active;
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getItemID() {
+		return itemID;
+	}
+
+
+	public void setItemID(String itemID) {
+		this.itemID = itemID;
+	}
+
+
+	public String getItemName() {
+		return itemName;
+	}
+
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+
+	public String getItemPrice() {
+		return itemPrice;
+	}
+
+
+	public void setItemPrice(String itemPrice) {
+		this.itemPrice = itemPrice;
+	}
+
+
+	public String getItemType() {
+		return itemType;
+	}
+
+
+	public void setItemType(String itemType) {
+		this.itemType = itemType;
+	}
+
+
+	public String getItemIMG() {
+		return itemIMG;
+	}
+
+
+	public void setItemIMG(String itemIMG) {
+		this.itemIMG = itemIMG;
+	}
+
+
+	public String getItemSeason() {
+		return itemSeason;
+	}
+
+
+	public void setItemSeason(String itemSeason) {
+		this.itemSeason = itemSeason;
+	}
+
+
+	public String getDesc() {
+		return desc;
+	}
+
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+
+	public String getSellerID() {
+		return sellerID;
+	}
+
+
+	public void setSellerID(String sellerID) {
+		this.sellerID = sellerID;
+	}
+
+
+	public String getSellerName() {
+		return sellerName;
+	}
+
+
+	public void setSellerName(String sellerName) {
+		this.sellerName = sellerName;
+	}
+
+
+	public String getSellerAddress() {
+		return sellerAddress;
+	}
+
+
+	public void setSellerAddress(String sellerAddress) {
+		this.sellerAddress = sellerAddress;
+	}
+
+
+	public String getSellerLatt() {
+		return sellerLatt;
+	}
+
+
+	public void setSellerLatt(String sellerLatt) {
+		this.sellerLatt = sellerLatt;
+	}
+
+
+	public String getSellerLongi() {
+		return sellerLongi;
+	}
+
+
+	public void setSellerLongi(String sellerLongi) {
+		this.sellerLongi = sellerLongi;
+	}
+
+
+	public String getSellerMob() {
+		return sellerMob;
+	}
+
+
+	public void setSellerMob(String sellerMob) {
+		this.sellerMob = sellerMob;
+	}
+
+
+	public String getViews() {
+		return views;
+	}
+
+
+	public void setViews(String views) {
+		this.views = views;
+	}
+
+
+	public boolean isActive() {
+		return active;
+	}
+
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	
+	
 }
