121de3112cc74d13d17bccfffc76f1e04edf97ff
soumyadip007 <soumyadip.note@gmail.com>
2020-02-02 13:01:55 +0530

    Entity BUG Fixed

--- AgricultureECommerce/src/main/java/com/ecommerce/agriculture/entity/Order.java
+++ AgricultureECommerce/src/main/java/com/ecommerce/agriculture/entity/Order.java
@@ -1,10 +1,14 @@
 package com.ecommerce.agriculture.entity;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "order")
 public class Order {
 	
 	
