848cc005c21bba0fafe78c618f6331a71c82a134
soumyadip007 <soumyadip.note@gmail.com>
2020-02-02 11:22:30 +0530

    orderID

---
+++ AgricultureECommerce/src/main/java/com/ecommerce/agriculture/entity/Order.java
@@ -0,0 +1,22 @@
+package com.ecommerce.agriculture.entity;
+
+public class Order {
+	
+	
+	orderid
+	
+	itemid
+	
+	orderprice
+	
+	orderqty
+	
+	sellerid
+	
+	userid
+	
+	userAddress
+	
+	string track
+	
+}
