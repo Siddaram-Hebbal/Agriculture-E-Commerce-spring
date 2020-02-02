810ad2ac89b09e9ac8435db33fda25aea982ff3c
soumyadip007 <soumyadip.note@gmail.com>
2020-02-02 11:50:21 +0530

    pkg rm

--- AgricultureECommerce/src/main/java/com/ecommerce/agriculture/repository/ItemRepository.java
+++ AgricultureECommerce/src/main/java/com/ecommerce/agriculture/repository/ItemRepository.java
@@ -3,7 +3,6 @@ package com.ecommerce.agriculture.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.ecommerce.agriculture.entity.Admin;
 import com.ecommerce.agriculture.entity.Item;
 
 /**
