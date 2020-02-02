56fca7b1d5bb2cbfe4d664e9884a08c38b827200
soumyadip007 <soumyadip.note@gmail.com>
2020-02-02 11:55:45 +0530

    Order

--- AgricultureECommerce/src/main/java/com/ecommerce/agriculture/repository/OrderRepository.java
+++ AgricultureECommerce/src/main/java/com/ecommerce/agriculture/repository/OrderRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.ecommerce.agriculture.entity.Item;
+import com.ecommerce.agriculture.entity.Order;
 
 /**
  * 
@@ -12,7 +13,7 @@ import com.ecommerce.agriculture.entity.Item;
  *
  */
 @Repository("orderRepository")
-public interface OrderRepository extends JpaRepository<Item, Integer> {
+public interface OrderRepository extends JpaRepository<Order, Integer> {
 
 	Item findByOrderid(int user);
 	
